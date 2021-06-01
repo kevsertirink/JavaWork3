893ef04b855186cfba695b741e6a2749acad2423
kevser tirink <85197428+kevsertirink@users.noreply.github.com>
2021-06-02 00:55:35 +0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,27 @@
+package javaCamp3;
+
+
+public class Student extends User {
+	
+	private String courses;
+
+	public Student() {
+		
+	}
+
+	public Student(String courses) {
+		
+		this.courses = courses;
+	}
+
+	public String getCourses() {
+		return courses;
+	}
+
+	public void setCourses(String courses) {
+		this.courses = courses;
+	}
+	
+	
+	
+}
